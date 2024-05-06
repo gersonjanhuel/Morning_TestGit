441afa915379efc529a5433a5aaad2337cd5e018
Gerson Janhuel <gersonjanhuel@gmail.com>
2024-05-06 11:36:53 +0700

    first commit...

---
+++ Morning_TestGit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Morning_TestGit
+//
+//  Created by Gerson Janhuel on 06/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Morning_TestGit/Morning_TestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Morning_TestGitApp.swift
+//  Morning_TestGit
+//
+//  Created by Gerson Janhuel on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Morning_TestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
